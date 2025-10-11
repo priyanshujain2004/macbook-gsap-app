4cda829e85d22df50322fb08434e6322da8edfc3
priyanshujain2004 <priyanshugaurav2004.com>
2025-10-12 01:14:57 +0530

    Implement 3D Product Viewer Section.

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,12 @@
 import {create} from 'zustand';
 
 const useMacbookStore = create((set) => ({
-    color: '#2c2e2c',
+    color: '#2e2e2e',
     setColor: (color) => set({color}),
 
     scale: 0.08,
     setScale: (scale) => set({scale}),
-    reset: () => set({color: '#2e2c3e', scale: 0.08}),
+    reset: () => set({color: '#2e2c2e', scale: 0.08}),
 }))
 
 export default useMacbookStore;
